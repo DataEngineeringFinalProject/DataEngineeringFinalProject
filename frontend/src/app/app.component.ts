7dc487ed0e45d159e9faf5f31d6cf079dc0e41c1
Lou <louise.bouque@gmail.com>
2022-02-15 12:26:58 +0100

    Add angular form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Toxicity Monitor';
+  formGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      sentence: ''
+    });
+  }
+
+  onSubmit(formData) {
+    var name = formData['sentence'];
+  }
 }
