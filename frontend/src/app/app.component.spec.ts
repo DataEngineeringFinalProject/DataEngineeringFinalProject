9bbda9bafac908592e0e3996fd62fdb1d9cc1808
CassandraLoupec <cassandra.loupec@efrei.net>
2022-02-27 15:00:41 +0100

    change test front

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,31 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { async, ComponentFixture,TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+        HttpTestingController
+       ],
     }).compileComponents();
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+    fixture.detectChanges()
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'Toxicity Monitor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('Toxicity Monitor');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+  it('form invalid when empty', () => {
+    expect(component.formGroup.valid).toBeFalsy();
   });
+
+  it('submitting a form emits a user', () => {
+    expect(component.formGroup.valid).toBeFalsy();
+    component.formGroup.controls['sentences'].setValue("I hate you");
+    expect(component.formGroup.valid).toBeTruthy();
+
+
+  });
+
+
 });
