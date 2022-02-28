54967a03452e7b51536ae9f41a5f5b35085fc88e
Lou <louise.bouque@gmail.com>
2022-02-28 11:27:52 +0100

    e2e test_v2

--- frontend/cypress/integration/test e2e/submit.spec.js
+++ frontend/cypress/integration/test e2e/submit.spec.js
@@ -8,16 +8,15 @@ describe('Toxicity', () => {
     cy.get('#Toxicity').should('be.empty')
     })
 
-    
     it('enter insult', () => {
     cy.get('#sentence').type('you are a cunt')
     cy.get('form').submit()
-    cy.get('#identity').should('not.be.empty')
-    cy.get('#insult').should('not.be.empty')
-    cy.get('#obscene').should('not.be.empty')
-    cy.get('#S_Toxicity').should('not.be.empty')
-    cy.get('#Threat').should('not.be.empty')
-    cy.get('#Toxicity').should('not.be.empty')
+    cy.get('#identity').should('not.be.empty').contains('Identity Attack :')
+    cy.get('#insult').should('not.be.empty').contains('Insult : ')
+    cy.get('#obscene').should('not.be.empty').contains('Obscene :')
+    cy.get('#S_Toxicity').should('not.be.empty').contains('Severe Toxicity :')
+    cy.get('#Threat').should('not.be.empty').contains('Threat :')
+    cy.get('#Toxicity').should('not.be.empty').contains('Toxicity :')
     })
  
 })
