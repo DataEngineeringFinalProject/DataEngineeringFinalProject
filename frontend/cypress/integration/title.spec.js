f7777f38c15919c5e6e8ceebe5c703b53511fafe
Lou <louise.bouque@gmail.com>
2022-03-02 23:42:25 +0100

    change title test with correct localhost

--- frontend/cypress/integration/title.spec.js
+++ frontend/cypress/integration/title.spec.js
@@ -1,6 +1,6 @@
 describe('Toxicity', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:4200/');
+      cy.visit('http://localhost:80/');
     });
   
     it('has the correct title', () => {
