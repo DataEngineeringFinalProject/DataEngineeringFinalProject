b6093bdad790ddf50a34ef907739974cf4c2e086
CassandraLoupec <cassandra.loupec@efrei.net>
2022-03-02 22:02:33 +0100

    api up prometheus

--- api/app.py
+++ api/app.py
@@ -1,12 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import numpy as np
 from detoxify import Detoxify
 import torch
+import time 
+from prometheus_client import CollectorRegistry, Summary, Counter, Gauge, Histogram, multiprocess, generate_latest,CONTENT_TYPE_LATEST
+from prometheus_flask_exporter import PrometheusMetrics
 
+#REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested')
+
+#EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
+
+#INPROGRESS = Gauge('hello_worlds_inprogress', 'Number of Hello Worlds in progress.')
+
+#LAST = Gauge('hello_world_last_time_seconds', 'The last time a Hello World was served.')
+
+#LATENCY = Histogram('hello_world_latency_seconds', 'Time for a request Hello World.', buckets=[0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1])
+#METRICS_REQUESTS = {Summary('request_processing_seconds', 'Time spent processing request')
+
+#,Counter('hello_worlds_total', 'Hello Worlds requested')
+
+#, Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
+
+#,Gauge('hello_worlds_inprogress', 'Number of Hello Worlds in progress.')
+
+#,Gauge('hello_world_last_time_seconds', 'The last time a Hello World was served.')
+
+#, Histogram('hello_world_latency_seconds', 'Time for a request Hello World.', buckets=[0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1])}
 
 def create_app():
     app = Flask(__name__)
     model = Detoxify('original')
+    #metrics = PrometheusMetrics(app)
 
     @app.route('/', methods=['GET', 'POST'])
     def makecalc():
@@ -21,8 +47,14 @@ def create_app():
             return jsonify(resultStringify)
         
         return "Not a proper request method or data"
+    
+    @app.route('/metrics')
+    def metrics():
+        REQUESTS.inc()
+        registry = CollectorRegistry()
+        multiprocess.MultiProcessCollector(registry)
+        return Response(generate_latest(registry), mimetype=CONTENT_TYPE_LATEST)
     return app
-
 
 
 if __name__ == '__main__':
