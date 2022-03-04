ea92ed05a0a45ee2d6b312bd557dab2aa281d26f
Maewinn <maud.glacee@gmail.com>
2022-03-04 17:53:40 +0100

    change default

--- backend/backend.js
+++ backend/backend.js
@@ -9,7 +9,7 @@ var app = express();
 const PORT = 3000;
 const corsOptions = {
     credentials: true,
-    origin: process.env.FRONTEND_URL || 'http://localhost:9080',
+    origin: process.env.FRONTEND_URL || 'http://front:80',
     optionsSuccessStatus: 200,
     method : 'POST,GET,PUT,OPTIONS,DELETE'
   }
